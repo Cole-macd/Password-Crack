5ce2408b6946f6519325237461a88a2fc9961561
connorwstein <connor.stein@mail.mcgill.ca>
2015-11-19 15:00:24 -0500

    working md5 digest

--- encrypt_decrypt.c
+++ encrypt_decrypt.c
@@ -1,53 +1,64 @@
 #include "encrypt_decrypt.h"
 
+
+
 void handleErrors(void)
 {
   ERR_print_errors_fp(stderr);
   abort();
 }
 
-int encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *key,
-  unsigned char *iv, unsigned char *ciphertext)
+int encrypt_digest(unsigned char *plaintext, int plaintext_len, unsigned char *hash, int hash_len, const EVP_MD *digest_encryption_algorithm){
+  EVP_MD_CTX ctx;
+  EVP_MD_CTX_init(&ctx);
+  EVP_DigestInit_ex(&ctx, digest_encryption_algorithm, NULL);
+  EVP_DigestUpdate(&ctx, plaintext, plaintext_len);
+  EVP_DigestFinal_ex(&ctx, hash, &hash_len);
+  EVP_MD_CTX_cleanup(&ctx);
+  return 0;
+}
+
+
+
+int encrypt_cipher(unsigned char *plaintext, int plaintext_len, unsigned char *key,
+  unsigned char *iv, unsigned char *ciphertext, const EVP_CIPHER *cipher_encryption_algorithm)
 {
   
-  EVP_CIPHER_CTX *ctx;
-
   int len;
-
   int ciphertext_len;
+  
+    EVP_CIPHER_CTX *ctx;
+    /* Create and initialise the context */
+    if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
 
-  /* Create and initialise the context */
-  if(!(ctx = EVP_CIPHER_CTX_new())) handleErrors();
-
-  /* Initialise the encryption operation. IMPORTANT - ensure you use a key
+    /* Initialise the encryption operation. IMPORTANT - ensure you use a key
    * and IV size appropriate for your cipher
    * In this example we are using 256 bit AES (i.e. a 256 bit key). The
    * IV size for *most* modes is the same as the block size. For AES this
-   * is 128 bits */
-  if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-    handleErrors();
+   * is 128 bits *///EVP_aes_256_cbc()
+    if(1 != EVP_EncryptInit_ex(ctx, cipher_encryption_algorithm, NULL, key, iv))
+      handleErrors();
+      /* Provide the message to be encrypted, and obtain the encrypted output.
+     * EVP_EncryptUpdate can be called multiple times if necessary
+     */
+    if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
+      handleErrors();
+    ciphertext_len = len;
 
-  /* Provide the message to be encrypted, and obtain the encrypted output.
-   * EVP_EncryptUpdate can be called multiple times if necessary
-   */
-  if(1 != EVP_EncryptUpdate(ctx, ciphertext, &len, plaintext, plaintext_len))
-    handleErrors();
-  ciphertext_len = len;
+    /* Finalise the encryption. Further ciphertext bytes may be written at
+     * this stage.
+     */
+    if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
+    ciphertext_len += len;
 
-  /* Finalise the encryption. Further ciphertext bytes may be written at
-   * this stage.
-   */
-  if(1 != EVP_EncryptFinal_ex(ctx, ciphertext + len, &len)) handleErrors();
-  ciphertext_len += len;
-
-  /* Clean up */
-  EVP_CIPHER_CTX_free(ctx);
+    /* Clean up */
+    EVP_CIPHER_CTX_free(ctx);
 
   return ciphertext_len;
 }
 
-int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
-  unsigned char *iv, unsigned char *plaintext)
+int decrypt_cipher(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
+  unsigned char *iv, unsigned char *plaintext, const EVP_CIPHER *cipher_encryption_algorithm)
 {
   EVP_CIPHER_CTX *ctx;
 
@@ -63,8 +74,9 @@ int decrypt(unsigned char *ciphertext, int ciphertext_len, unsigned char *key,
    * In this example we are using 256 bit AES (i.e. a 256 bit key). The
    * IV size for *most* modes is the same as the block size. For AES this
    * is 128 bits */
-  if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_cbc(), NULL, key, iv))
-    handleErrors();
+  if(1 != EVP_DecryptInit_ex(ctx, cipher_encryption_algorithm, NULL, key, iv))
+       handleErrors();
+  
 
   /* Provide the message to be decrypted, and obtain the plaintext output.
    * EVP_DecryptUpdate can be called multiple times if necessary
