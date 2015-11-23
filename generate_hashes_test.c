42ea87c2b7623705fc76e297e7c3b94ba76cd83e
connorwstein <connor.stein@mail.mcgill.ca>
2015-11-23 11:30:03 -0500

    working sha1

--- generate_hashes_test.c
+++ generate_hashes_test.c
@@ -1,6 +1,13 @@
 #include "encrypt_decrypt.h"
 #include <string.h>
 
+#define SHA1_HASH_SIZE 20
+#define MD5_HASH_SIZE 16
+
+//For block ciphers, the input size is the output size
+//BUT the input size must be a multiple of the blocks (or padding is needed)
+//Block size for AES is 128 bits (16 bytes), but the key size can be 128, 192 or 256 bits
+//Looks likes EVP takes care of this padding though
 void test_aes_256_cbc(void){
   /* A 256 bit key */
   unsigned char *key = (unsigned char *)"01234567890123456789012345678901";
@@ -45,23 +52,63 @@ void test_aes_256_cbc(void){
   }
 }
 
+// Returns a string representing the command line output froming running "cmd"
+char* get_command_output(char* cmd, int max_output) {
+    char* data = malloc(sizeof(char)*max_output);
+    memset(data, 0, sizeof(data));
+    FILE* stream = popen(cmd, "r");
+    if (stream) {
+        if (fgets(data, max_output, stream) == NULL){
+            printf("Failed to get output\n");
+        } 
+        pclose(stream);
+    }
+    else{
+      printf("Failed to open process %s\n",cmd);
+    }
+    return data;
+}
 
 void test_md5(void){
   char plaintext[]= "Hello world";
   //md5 produces a 16 byte hash value (one way)
-  unsigned char hash[16]; // results
+  unsigned char hash[MD5_HASH_SIZE]; 
   memset(hash, 0, sizeof(hash));
-  const EVP_MD *md = EVP_get_digestbyname("MD5");
-  if(!md) printf("Unable to initialize MD5\n");
   encrypt_digest(plaintext, strlen(plaintext), hash, 0, EVP_md5());
   // Below output should be the same as echo -n "Hello world" | openssl md5
-  printf("MD5(%s)=", plaintext);
+  char buffer[256];
+  memset(buffer, 0, sizeof(buffer));
+  sprintf(buffer, "(stdin)= ");
   int i;
-  for(i = 0; i < 16; i++){
-    printf("%02x", (unsigned char)hash[i]);
+  for(i = 0; i < MD5_HASH_SIZE; i++){
+    sprintf(buffer+9+2*i, "%02x", hash[i]);
   }
-  printf("\n");
+  sprintf(buffer+9+2*i, "\n");
+  char *linux_call = get_command_output("echo -n \"Hello world\" | openssl md5 2>&1", 256);
+  if(strcmp(linux_call, buffer) == 0){
+    printf(" MD5 PASSED\n");
+  }
+}
 
+
+void test_sha_1(void){
+  char plaintext[]= "Hello world";
+  // SHA1 gives a 20 byte hash
+  unsigned char hash[SHA1_HASH_SIZE]; 
+  memset(hash, 0, sizeof(hash));
+  encrypt_digest(plaintext, strlen(plaintext), hash, 0, EVP_sha1());
+  char buffer[256];
+  memset(buffer, 0, sizeof(buffer));
+  sprintf(buffer, "(stdin)= ");
+  int i;
+  for(i = 0; i < SHA1_HASH_SIZE; i++){
+    sprintf(buffer+9+2*i, "%02x", hash[i]);
+  }
+  sprintf(buffer+9+2*i, "\n");
+  char *linux_call = get_command_output("echo -n \"Hello world\" | openssl sha1 2>&1", 256);
+  if(strcmp(linux_call, buffer) == 0){
+    printf(" SHA1 PASSED\n");
+  }
 
 }
 
@@ -80,7 +127,7 @@ int main(void){
   // with the same key
   test_aes_256_cbc();
   test_md5();
-  
+  test_sha_1();
   /* Clean up */
   EVP_cleanup();
   ERR_free_strings();
